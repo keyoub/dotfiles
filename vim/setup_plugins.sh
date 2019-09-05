61241df9edb1b22c5fec4849f3a434fb8f013d47
Bardia Keyoumarsi <bardia@easypost.com>
2019-09-05 00:43:16 +0000

    vim-go pin

--- vim/setup_plugins.sh
+++ vim/setup_plugins.sh
@@ -7,11 +7,20 @@ while read plugin; do
     git clone $plugin 2>/dev/null
 done < $1
 
+# pin vim-go on 1.10 since recent versions break
+# go env pickup
+pushd ./vim-go
+git checkout v1.10 #2>/dev/null
+popd
+
 # update any existing ones
 for plugin in */; do
-    pushd $plugin
-    git pull
-    popd
+    if [ "$plugin" != "vim-go/" ]
+    then
+        pushd $plugin
+        git pull
+        popd
+    fi
 done
 
 popd
