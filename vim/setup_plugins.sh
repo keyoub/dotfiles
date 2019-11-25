25c6e0dcc1dafb6ee0a726661976dd47e41af27f
Bardia Keyoumarsi <bardia@easypost.com>
2019-11-25 22:29:13 +0000

    Remove the vim-go v10 pin

--- vim/setup_plugins.sh
+++ vim/setup_plugins.sh
@@ -7,11 +7,12 @@ while read plugin; do
     git clone $plugin 2>/dev/null
 done < $1
 
+# They seem to have fixed the master but keeping this handy
 # pin vim-go on 1.10 since recent versions break
 # go env pickup
-pushd ./vim-go
-git checkout v1.10 #2>/dev/null
-popd
+# pushd ./vim-go
+# git checkout v1.10 #2>/dev/null
+# popd
 
 # update any existing ones
 for plugin in */; do
